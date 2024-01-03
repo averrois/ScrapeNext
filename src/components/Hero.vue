4806eb4317187f69dcb32e835353259ea97cb87e
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-03 11:01:26 +0100

    feat: update the structure of Hero Component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,27 +5,48 @@
 <template>
     <div class="px-4 md:block">
         <div class="pb-8 mt-auto">
-            <div class="text-center">
-                <h1 class="text-2xl md:text-3xl mb-8 mt-16 font-serif max-w-3xl mx-auto">The Progressive Python <span
-                        class="text-indigo-600">Framework</span> For Trading Cryptocurrencies</h1>
+            <div class="text-center max-w-4xl flex flex-col justify-center items-center m-auto">
+                <h1 class="text-4xl md:text-7xl mb-8 mt-16 font-serif font-light max-w-4xl mx-auto text-white-800">
+                    The Progressive Empower Your Projects with
+                    <span class="text-primary font-normal">ScrapeNext</span>
+                </h1>
 
-                <div class="md:max-w-2xl mx-auto">
-                    <p class="text-gray-800">
-                        Jesse is not merely another bot. It is a framework focusing on helping you develop your very own
-                        trading strategies.
+                <div class="md:max-w-4xl mx-auto">
+                    <p class="text-white-500 md:text-lg">
+                        Your go-to solution for effortlessly collecting and managing data from the web. Explore the endless
+                        possibilities with ScrapeNest - Simplifying Web Scraping for You.
                     </p>
                 </div>
 
-                <div class="mb-6 mt-12">
-                    <a href="#"
-                        class="py-2 px-4 md:py-4 md:px-6 mr-2 border select-none hover:bg-indigo-400 bg-indigo-500 rounded shadow-lg text-white">Why
-                        Jesse?</a>
+                <div class="mb-6 mt-12 w-full">
 
-                    <a href="#"
-                        class="py-2 px-4 md:py-4 md:px-6 border select-none hover:bg-gray-100 bg-white rounded shadow-lg text-indigo-800">
-                        Get Started <span>&rarr;</span> </a>
+                    <form>
+                        <label for="default-search"
+                            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                </svg>
+                            </div>
+                            <input type="search" id="default-search"
+                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                                placeholder="paste url here..." required>
+                            <button type="submit"
+                                class="text-white absolute end-2.5 bottom-2.5 bg-primary focus:outline-none font-medium rounded text-sm px-6 py-2">Search</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+h1 {
+    line-height: 4rem;
+}
+</style>
