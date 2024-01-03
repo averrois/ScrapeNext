ab6c48f8322f284b3a6fe7c2f14ae72d54c7828a
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-03 16:45:19 +0100

    feat(Searchbar): adding submit event handlers to save value of search input

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchPrompt = ref('')
+
+
+const handleSubmit = (e: Event) => {
+    e.preventDefault()
+    console.log(searchPrompt.value)
+}
+
+
+const handleSearchValue = (e: Event) => {
+    searchPrompt.value = (e.target as HTMLInputElement).value;
+}
+
+</script>
+
 <template>
-    <form>
+    <form @submit="handleSubmit">
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -9,11 +27,18 @@
                         d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input type="search" id="default-search"
+            <input type="text"
                 class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-                placeholder="paste url here..." required>
-            <button type="submit"
-                class="text-white absolute end-2.5 bottom-2.5 bg-primary focus:outline-none font-medium rounded text-sm px-6 py-2">Search</button>
+                placeholder="paste url here..." 
+                required 
+                v-model="searchPrompt" 
+                @input="handleSearchValue" />
+            <button 
+                type="submit"
+                class="text-white absolute end-2.5 bottom-2.5 bg-primary focus:outline-none font-medium rounded text-sm px-6 py-2"
+            >
+                Search
+            </button>
         </div>
     </form>
 </template>
