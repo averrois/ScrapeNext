53d2b429c7f130b25ae3e4b430c47c22df3ab806
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-04 14:06:49 +0100

    feat: fetch currentPrice, title from the scraped product

--- src/lib/scraper/index.ts
+++ src/lib/scraper/index.ts
@@ -1,28 +1,35 @@
 import axios from "axios"
-import * as cheerio  from "cheerio"
+import * as cheerio from "cheerio"
+import { extractPrice } from "../utils"
 
 export async function scraperAmazonProduct(url: string) {
-  if (!url) return
+    if (!url) return
 
-  const backendUrl = "http://localhost:3000/api"
+    const backendUrl = "http://localhost:3000/api"
 
-  try {
-    const response = await axios.get(backendUrl, {
-      params: { url }, // Send the original URL as a parameter to backend
-    })
+    try {
+        const response = await axios.get(backendUrl, {
+            params: { url }, // Send the original URL as a parameter to backend
+        })
+        
+        // Handle the response as needed
+        
+        // Initialize Cheerio
+        const $ = cheerio.load(response.data)
 
-    // Handle the response as needed
-    // console.log(response.data)
-    // Initialize Cheerio
-    const $ = cheerio.load(response.data)
+        // Extract product title
+        const title = $('#productTitle').text().trim()
+        // Extract product price
+        const currentPrice = extractPrice(
+            $('.priceToPay span.a-price-whole'),
+            $('.a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+        )
 
-    // Extract product title
-    const title = $('#productTitle').text().trim()
+        console.log( currentPrice)
 
-    console.log(title)
-
-  } catch (error: any) {
-    console.error('Failed to fetch:', error.message)
-    // Handle the error appropriately
-  }
+    } catch (error: any) {
+        console.error('Failed to fetch:', error.message)
+        // Handle the error appropriately
+    }
 }
