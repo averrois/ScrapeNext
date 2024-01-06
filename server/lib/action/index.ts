cabce67aeaa845586377db9117f9a889a8c56262
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-06 22:00:37 +0100

    feat: add action directory to organize code structure

---
+++ server/lib/action/index.ts
@@ -0,0 +1,43 @@
+// import { connectToDB } from "../database";
+// import Product from "../database/models/product.model";
+// import { scrapeAmazonProduct } from "../scrape";
+// import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+
+// export async function scrapeAndStoreProduct(productUrl: string) {
+//     if (!productUrl) return;
+
+//     try {
+//         connectToDB();
+
+//         const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+//         if (!scrapedProduct) return;
+
+//         let product = scrapedProduct;
+
+//         const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+//         if (existingProduct) {
+//             const updatedPriceHistory: any = [
+//                 ...existingProduct.priceHistory,
+//                 { price: scrapedProduct.currentPrice }
+//             ]
+
+//             product = {
+//                 ...scrapedProduct,
+//                 priceHistory: updatedPriceHistory,
+//                 lowestPrice: getLowestPrice(updatedPriceHistory),
+//                 highestPrice: getHighestPrice(updatedPriceHistory),
+//                 averagePrice: getAveragePrice(updatedPriceHistory),
+//             }
+//         }
+
+//         const newProduct = await Product.findOneAndUpdate(
+//             { url: scrapedProduct.url },
+//             product,
+//             { upsert: true, new: true }
+//         );
+//     } catch (error: any) {
+//         throw new Error(`Failed to create/update product: ${error.message}`)
+//     }
+// }
