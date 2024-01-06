daa4f89f70d4969189dd7d3b463220c869a1b9f7
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-06 11:54:38 +0100

    refactor: export that data from the response

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import axios from 'axios';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import * as cheerio from 'cheerio';
+import { extractCurrency, extractPrice } from "./lib/utils"
 
 dotenv.config({ path: '../.env' })
 const app = express();
@@ -62,7 +64,69 @@ app.use('/api', async (req: Request, res: Response) => {
 
     const brightDataResponse = await axios.get(url, options);
     res.json(brightDataResponse.data);
-    console.log(brightDataResponse.data)
+
+    // Handle the response as needed
+
+    // Initialize Cheerio
+    const $ = cheerio.load(brightDataResponse.data)
+
+    // Extract product title
+    const title = $('#productTitle').text().trim()
+
+    // Extract product current price (discount)
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('.a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base'),
+    )
+
+    // // Extract original
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+      $('#listPrice'),
+      $('#priceblock_dealprice'),
+      $('.a-size-base.a-color-price')
+    )
+
+    // Extract if the product is out of stack
+    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable'
+
+    // Extract images & oarsing them
+    const images =
+      $('#imgBlkFront').attr('data-a-dynamic-image') ||
+      $('#landingImage').attr('data-a-dynamic-image') ||
+      '{}'
+
+    const imageUrls = Object.keys(JSON.parse(images))
+
+    // Extract currency 
+    const currency = extractCurrency($('.a-price-symbol'))
+
+    // Extract discount percentage
+    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "")
+
+    // Construct data object with scraped information
+    const data = {
+      url,
+      currency: currency || '$',
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      isOutOfStock: outOfStock,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+      // Other elements to add letter on
+      category: 'category',
+      reviewsCount: 10,
+      stars: 3,
+    }
+
+    console.log(data)
   } catch (error: any) {
     console.error('Error:', error.message);
     res.status(500).json({ error: 'Internal server error' });
