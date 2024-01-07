322bba3898f2a60685a3959e1c6abb0781b59b97
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-07 22:44:09 +0100

    feat: update ProductView

--- client/src/views/ProductView.vue
+++ client/src/views/ProductView.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
-import Card from '@/components/Card.vue';
+import Card from '@/components/Card.vue'
+import axios from 'axios'
+import { ref, onMounted } from 'vue'
+
+const products = ref(null)
+
+const fetchProducts = async () => {
+    try {
+        const response = await axios.get('/api/products')
+        products.value = response.data
+        console.log(products.value)
+    } catch (error: any) {
+        console.log(error)
+        error.value = 'Error fetching products'
+    }
+}
+
+onMounted(fetchProducts)
 
 
 </script>
@@ -7,5 +24,9 @@ import Card from '@/components/Card.vue';
 <template>
     <div class="md:max-w[1000px] md:p-2 flex flex-wrap justify-around items-center m-auto">
         <Card />
+        <ul v-if="products">
+            <li>Nice</li>
+        </ul>
+        <p v-else>Loading products...</p>
     </div>
 </template>
