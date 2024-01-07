0450b1402cb8e7d686324b627945f05e9fc41a6e
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-07 09:39:36 +0100

    feat(views): add PorductView

---
+++ client/src/views/ProductView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Card from '@/components/Card.vue';
+
+
+</script>
+
+<template>
+    <div class="md:max-w[1000px] md:p-2 flex flex-wrap justify-around items-center m-auto">
+        <Card />
+    </div>
+</template>
