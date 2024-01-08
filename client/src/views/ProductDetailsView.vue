f015e390905e0039b6e90da2dcc8dffc2d34072b
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-08 23:08:22 +0100

    wip: face error on fetch product to from client side

--- client/src/views/ProductDetailsView.vue
+++ client/src/views/ProductDetailsView.vue
@@ -13,12 +13,17 @@ const fetchProductDetails = async () => {
         console.log(productId)
         const response = await axios.get(`http://localhost:3000/api/products/${productId}`)
         product.value = await response.data
-        console.log(product.value)
     } catch (error: any) {
         console.error(error);
         // Handle error...
     }
 }
+
+const truncateTitle = (title: string, maxLength: number) => {
+    // if (title.length < maxLength) return ''
+    return title.length > maxLength ? title.substring(0, maxLength) + '...' : title;
+}
+
 onMounted(fetchProductDetails)
 
 
@@ -28,7 +33,8 @@ onMounted(fetchProductDetails)
     <section class="container mx-auto px-4 md:px-6 py-8">
         <div class="grid lg:grid-cols-2 gap-6 items-start">
             <div>
-                <img alt="Product Image" class="rounded-lg object-cover w-full aspect-square" height="600" src=""
+                <img alt="Product Image" class="rounded-lg object-cover w-full aspect-square" height="600" 
+                :src="product?.image"
                     width="600" />
                 <!-- Mini other images  -->
                 <!-- <div class="grid grid-cols-4 gap-2 mt-4">
@@ -41,14 +47,13 @@ onMounted(fetchProductDetails)
                 </div> -->
             </div>
             <div>
-                <h1 class="text-3xl font-bold">Product Name</h1>
+                <h1 class="text-3xl font-bold text-white">{{ truncateTitle(product.title, 60) }}</h1>
                 <div className="my-7 flex flex-col gap-5">
                     <div className="flex gap-5 flex-wrap">
-                        <h1>{{ product.title }}</h1>
-                        <!-- <PriceInfoCard 
+                        <PriceInfoCard 
                             title="Current Price" 
-                            price="product.currentPrice" 
-                            currency="product.currency" /> -->
+                            :price="product?.currentPrice" 
+                            :currency="product?.currency" />
                     </div>
                 </div>
             </div>
