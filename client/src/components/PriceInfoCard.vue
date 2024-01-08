706fe6ca622842e06ee070aaffeb5c365008adb0
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-08 20:56:54 +0100

    feat: update PriceInfoCard component

--- client/src/components/PriceInfoCard.vue
+++ client/src/components/PriceInfoCard.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-    <div class="flex-1 min-w-[200px] flex flex-col gap-2 border-black-300 border-2 rounded-md bg-gradient-to-r from-black-200  to-black-100   px-5 py-4">
+    <div class="flex-1 min-w-[200px] flex flex-col gap-2 border-black-300 border-[3px] rounded-md  px-5 py-4">
       <p class="text-xl text-white">Test price</p>
 
       <div class="flex gap-1">
         <img src="../assets/icons/price-tag.svg" alt="test" width="24" height="24" />
 
-        <p class="text-2xl font-bold text-secondary">00</p>
+        <p class="text-2xl font-bold text-white">00</p>
       </div>
     </div>
 </template>
