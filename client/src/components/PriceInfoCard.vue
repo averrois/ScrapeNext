fa6dc5f2c1bcecb49fe3c77609f97c4f8f5fb05b
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-08 20:52:56 +0100

    cleanup: remove unused commented code

--- client/src/components/PriceInfoCard.vue
+++ client/src/components/PriceInfoCard.vue
@@ -3,17 +3,6 @@
 </script>
 
 <template>
-    <!-- <button
-        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none flex items-center gap-3"
-        type="button">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-            class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z">
-            </path>
-        </svg>
-        Add to Bookmark
-    </button> -->
     <div class="flex-1 min-w-[200px] flex flex-col gap-2 border-black-300 border-2 rounded-md bg-gradient-to-r from-black-200  to-black-100   px-5 py-4">
       <p class="text-xl text-white">Test price</p>
 
