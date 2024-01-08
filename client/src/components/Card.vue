4c7995e8a886d868a52fd806fc75d7fd887c1871
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-08 22:29:01 +0100

    feat: update client side component the send requests to server

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -32,10 +32,10 @@ const truncateTitle = (title: string, maxLength: number) => {
                     </span>
                 </p>
             </div>
-            <a href="#"
+            <router-link :to="`/products/${product._id}`"
                 class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
                 Add to cart
-            </a>
+            </router-link>
         </div>
     </div>
 </template>
