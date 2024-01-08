e0dc6ffc8355b7ce4c0018ed6fa680b4a4efb560
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-08 17:26:05 +0100

    feat: update Card Cmponent

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 defineProps(['product'])
 
+const truncateTitle = (title: string, maxLength: number) => {
+    return title.length > maxLength ? title.substring(0, maxLength) + '...' : title;
+}
 </script>
 
 <template>
@@ -15,7 +18,7 @@ defineProps(['product'])
         </a>
         <div class="mt-4 px-5 pb-5">
             <a href="#">
-                <h5 class="text-xl tracking-tight text-slate-900">{{ product.title }}</h5>
+                <h5 class="text-xl tracking-tight font-bold text-slate-900">{{ truncateTitle(product.title, 30) }}</h5>
             </a>
             <div class="mt-2 mb-5 flex items-center justify-between">
                 <p>
@@ -31,7 +34,8 @@ defineProps(['product'])
             </div>
             <a href="#"
                 class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-                Add to cart 
+                Add to cart
             </a>
+        </div>
     </div>
-</div></template>
+</template>
