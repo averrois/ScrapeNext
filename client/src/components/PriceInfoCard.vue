33436f805b0416de1f8b7b1af9c0ee090d2f32da
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-08 21:05:55 +0100

    feat: update PriceInfoCard and add props

--- client/src/components/PriceInfoCard.vue
+++ client/src/components/PriceInfoCard.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
 
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+  currency: {
+    type: String,
+    default: '',
+  },
+})
+
 </script>
 
 <template>
     <div class="flex-1 min-w-[200px] flex flex-col gap-2 border-black-300 border-[3px] rounded-md  px-5 py-4">
-      <p class="text-xl text-white">Test price</p>
+        <p class="text-xl text-white">{{ title }}</p>
 
-      <div class="flex gap-1">
-        <img src="../assets/icons/price-tag.svg" alt="test" width="24" height="24" />
+        <div class="flex gap-1">
+            <img src="../assets/icons/price-tag.svg" alt="test" width="24" height="24" />
 
-        <p class="text-2xl font-bold text-white">00</p>
-      </div>
+            <p class="text-2xl font-bold text-white">{{ currency }} {{ price }}</p>
+        </div>
     </div>
 </template>
