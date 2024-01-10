3d6b77687a879483c82cf3c7e9b175b92083c844
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-10 21:49:13 +0100

    wip: face error to redirect a user after search action on the home page

--- server/index.ts
+++ server/index.ts
@@ -72,11 +72,6 @@ app.use('/api', async (req: Request, res: Response) => {
         highestPrice: getHighestPrice(updatedPriceHistory),
         averagePrice: getAveragePrice(updatedPriceHistory),
       }
-
-      // Assuming you have the product details available in the 'product' variable
-      res.status(200).json({ message: 'Product saved successfully', product });
-      // Redirect to the product details page
-      res.redirect(`/products/${product._id}`);
     }
 
     const newProduct = await Product.findOneAndUpdate(
