6804e4c25c98dbc6bcaa735c46c004e9c3674d1a
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-11 22:54:08 +0100

    wip: adding laoding animation

--- client/src/components/Searchbar.vue
+++ client/src/components/Searchbar.vue
@@ -70,16 +70,12 @@ const handleSearchValue = (e: Event) => {
 
 <template>
     <div class="absolute w-full h-full z-20 left-0 top-0 flex justify-center items-center">
-        <div class="loader z-10">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div class="before_laoder"></div>
+        <div class="content z-10">
+            <div class="loading flex justify-center items-center">
+                <p class="text-2xl mb-6 font-medium uppercase">loading</p>
+                <span></span>
+            </div>
         </div>
     </div>
     <form @submit="handleSubmit">
@@ -109,7 +105,7 @@ const handleSearchValue = (e: Event) => {
     backdrop-filter: blur(10px);
 }
 
-.loader::before {
+.before_laoder {
     content: '';
     display: block;
     position: absolute;
@@ -120,9 +116,139 @@ const handleSearchValue = (e: Event) => {
     background-color: rgba(0, 0, 0, 0.562);
     /* Adjust the darkness level here */
     z-index: 1;
+    backdrop-filter: blur(5px);
 }
 
-.loader {
+
+
+.content {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.content .loading {
+    width: 80px;
+    height: 50px;
+    position: relative;
+}
+
+.content .loading p {
+    top: 0;
+    color: #2190FF;
+    font-family: "Oxygen", sans-serif;
+    animation: text 3.5s ease both infinite;
+    /* font-size: 12px; */
+    letter-spacing: 1px;
+}
+
+@keyframes text {
+    0% {
+        letter-spacing: 1px;
+        transform: translateX(0px);
+    }
+
+    40% {
+        letter-spacing: 2px;
+        transform: translateX(26px);
+    }
+
+    80% {
+        letter-spacing: 1px;
+        transform: translateX(32px);
+    }
+
+    90% {
+        letter-spacing: 2px;
+        transform: translateX(0px);
+    }
+
+    100% {
+        letter-spacing: 1px;
+        transform: translateX(0px);
+    }
+}
+
+.content .loading span {
+    background-color: #2190FF;
+    border-radius: 50px;
+    display: block;
+    height: 16px;
+    width: 16px;
+    bottom: 0;
+    position: absolute;
+    transform: translateX(64px);
+    animation: loading 3.5s ease both infinite;
+}
+
+.content .loading span:before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: #101E42;
+    border-radius: inherit;
+    animation: loading2 3.5s ease both infinite;
+}
+
+@keyframes loading {
+    0% {
+        width: 16px;
+        transform: translateX(0px);
+    }
+
+    40% {
+        width: 100%;
+        transform: translateX(0px);
+    }
+
+    80% {
+        width: 16px;
+        transform: translateX(64px);
+    }
+
+    90% {
+        width: 100%;
+        transform: translateX(0px);
+    }
+
+    100% {
+        width: 16px;
+        transform: translateX(0px);
+    }
+}
+
+@keyframes loading2 {
+    0% {
+        transform: translateX(0px);
+        width: 16px;
+    }
+
+    40% {
+        transform: translateX(0%);
+        width: 80%;
+    }
+
+    80% {
+        width: 100%;
+        transform: translateX(0px);
+    }
+
+    90% {
+        width: 80%;
+        transform: translateX(15px);
+    }
+
+    100% {
+        transform: translateX(0px);
+        width: 16px;
+    }
+}
+
+
+/* .loader {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 8px;
@@ -135,7 +261,7 @@ const handleSearchValue = (e: Event) => {
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #66cdaa;
+    background-color: #2190FF;
     border-radius: 50%;
 }
 
@@ -149,12 +275,12 @@ const handleSearchValue = (e: Event) => {
 }
 
 .loader div:before {
-    background-color: #ffdab9;
+    background-color: #05070D;
     animation: scale-1 2400ms linear infinite;
 }
 
 .loader div:after {
-    background-color: #66cdaa;
+    background-color: #2190FF;
     animation: scale-2 2400ms linear infinite;
 }
 
@@ -292,4 +418,4 @@ const handleSearchValue = (e: Event) => {
     100% {
         transform: scale(1);
     }
-}</style>
+} */</style>
