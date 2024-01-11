a65b95ca9b027e390292b4bb8d9c5cb7e7a52a61
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-11 12:53:48 +0100

    feat: make the product view price card info responsive

--- client/src/views/ProductDetailsView.vue
+++ client/src/views/ProductDetailsView.vue
@@ -27,10 +27,10 @@ onMounted(fetchProductDetails)
 
 <template>
     <section class="container mx-auto px-4 md:px-6 py-8">
-        <div class="grid lg:grid-cols-2 gap-6 items-start">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 items-start">
             <div>
-                <img alt="Product Image" class="rounded-lg object-cover w-full aspect-square" height="600"
-                    :src="product?.image" width="600" />
+                <img alt="Product Image" class="rounded-lg object-cover w-full aspect-square mb-4 md:mb-0"
+                    :src="product?.image" />
                 <!-- Mini other images  -->
                 <!-- <div class="grid grid-cols-4 gap-2 mt-4">
                     <img alt="Product Thumbnail"
@@ -42,35 +42,17 @@ onMounted(fetchProductDetails)
                 </div> -->
             </div>
             <div>
-                <h1 class="text-3xl font-bold text-white truncate">{{ product?.title }}</h1>
-                <div className="my-7 flex flex-col gap-5">
-                    <div className="flex gap-5 flex-wrap">
-                        <PriceInfoCard title="Current Price" :price="product?.currentPrice" :currency="product?.currency" />
-                        <PriceInfoCard 
-                            v-if="product?.discountRate" 
-                            title="Original Price" 
-                            :price="product?.originalPrice"
-                            :currency="product?.currency" 
-                        />
-                        <PriceInfoCard 
-                            v-if="product?.lowestPrice" 
-                            title="Lowest Price" 
-                            :price="product?.lowestPrice"
-                            :currency="product?.currency" 
-                        />
-                        <PriceInfoCard 
-                            v-if="product?.highestPrice" 
-                            title="Highest Price" 
-                            :price="product?.highestPrice"
-                            :currency="product?.currency" 
-                        />
-                        <PriceInfoCard 
-                            v-if="product?.averagePrice" 
-                            title="Average  Price" 
-                            :price="product?.averagePrice"
-                            :currency="product?.currency" 
-                        />
-                    </div>
+                <h1 class="text-3xl font-bold text-white truncate mb-4">{{ product?.title }}</h1>
+                <div class="flex flex-wrap gap-5">
+                    <PriceInfoCard title="Current Price" :price="product?.currentPrice" :currency="product?.currency" />
+                    <PriceInfoCard v-if="product?.discountRate" title="Original Price" :price="product?.originalPrice"
+                        :currency="product?.currency" />
+                    <PriceInfoCard v-if="product?.lowestPrice" title="Lowest Price" :price="product?.lowestPrice"
+                        :currency="product?.currency" />
+                    <PriceInfoCard v-if="product?.highestPrice" title="Highest Price" :price="product?.highestPrice"
+                        :currency="product?.currency" />
+                    <PriceInfoCard v-if="product?.averagePrice" title="Average  Price" :price="product?.averagePrice"
+                        :currency="product?.currency" />
                 </div>
             </div>
         </div>
