29bf624b5b9e079b6c138603fe8b0973595604a4
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-12 23:40:05 +0100

    wip: adding model component

--- client/src/components/Model.vue
+++ client/src/components/Model.vue
@@ -4,19 +4,21 @@ import { ref } from 'vue';
 const isClose = ref(false)
 
 const handleClose = () => {
-    isClose.value = true
+    console.log(isClose.value)
+    isClose.value = !isClose.value
+    console.log(isClose.value)
 }
 
 </script>
 
 <template>
-    <div :class="{'hidden': isClose}" class="absolute w-full h-full z-20 left-0 top-0 flex justify-center items-center">
+    <div v-if="!isClose" class="absolute w-full h-full z-20 left-0 top-0 flex justify-center items-center">
         <div class="blur_bg"></div>
         <div class="fixed inset-0 flex items-center justify-center z-50 m-6" :class="{ 'hidden': isClose }">
             <div class="bg-black-200 border-black-300 border-2 rounded-lg shadow-lg p-6 w-full max-w-md mx-auto">
                 <div class="flex justify-between items-center">
                     <h2 class="text-white text-2xl font-bold">Track This Product!</h2>
-                    <button class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
+                    <button @click="handleClose" class="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                             class="h-5 w-5">
