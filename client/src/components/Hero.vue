04010ac31bcde407b5f5567ff70979edcd097624
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-18 20:59:19 +0100

    cleanup: remove unused code

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -3,31 +3,6 @@ import Searchbar from "./Searchbar.vue";
 </script>
 
 <template>
-  <!-- <div class="px-4 md:block">
-    <div class="pb-8 mt-auto max-md:pc-4">
-      <div
-        class="text-center max-w-4xl flex flex-col justify-center items-center m-auto"
-      >
-        <h1
-          class="text-5xl md:text-7xl mb-8 mt-16 font-extrabold max-w-4xl mx-auto text-white-800"
-        >
-          Unleash the Scraping Magic for Your Products with
-          <span class="text-primary font-normal">ScrapeNext</span>
-        </h1>
-        <div class="md:max-w-4xl mx-auto">
-          <p class="text-white-500 text-2xl max-md:text-xl font-regular">
-            Your go-to solution for effortlessly collecting and managing data
-            from the web. Explore the endless possibilities with ScrapeNest -
-            Simplifying Web Scraping for You.
-          </p>
-        </div>
-
-        <div class="mb-6 mt-12 w-full">
-          <Searchbar />
-        </div>
-      </div>
-    </div>
-  </div> -->
   <section class="w-full h-screen py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
     <div class="container px-4 md:px-6">
       <div class="grid gap-6 items-center">
@@ -46,31 +21,7 @@ import Searchbar from "./Searchbar.vue";
               Simplifying Web Scraping for You.
             </p>
           </div>
-          <div class="w-full max-w-xl space-y-2 mx-auto">
-            <form class="flex space-x-2">
-              <input
-                type="email"
-                class="flex h-10 w-full rounded-md border px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 max-w-lg flex-1 bg-gray-800 text-white border-gray-900"
-                placeholder="Enter your email"
-              />
-              <button
-                class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-gray-800/90 hover:text-white h-10 px-4 py-2 bg-white text-black"
-                type="submit"
-              >
-                Join Now
-              </button>
-            </form>
-            <p class="text-xs text-zinc-200 dark:text-zinc-100">
-              Get ready to redefine your email experience.
-              <a
-                class="underline underline-offset-2 text-white"
-                href="#"
-                rel="ugc"
-              >
-                Terms &amp; Conditions
-              </a>
-            </p>
-          </div>
+          <Searchbar />
         </div>
       </div>
     </div>
--- client/src/components/Searchbar.vue
+++ client/src/components/Searchbar.vue
@@ -54,9 +54,6 @@ const handleSubmit = async (e: Event) => {
   }
 };
 
-const handleSearchValue = (e: Event) => {
-  searchPrompt.value = (e.target as HTMLInputElement).value;
-};
 </script>
 
 <template>
@@ -72,50 +69,6 @@ const handleSearchValue = (e: Event) => {
       </div>
     </div>
   </div>
-  <!-- <form @submit="handleSubmit">
-    <label
-      for="default-search"
-      class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-      >Search</label
-    >
-    <div class="relative">
-      <div
-        class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
-      >
-        <svg
-          class="w-4 h-4 text-gray-500 dark:text-gray-400"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 20"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-          />
-        </svg>
-      </div>
-      <input
-        type="text"
-        class="block w-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-        placeholder="paste url here..."
-        required
-        v-model="searchPrompt"
-        @input="handleSearchValue"
-      />
-      <button
-        type="submit"
-        :disabled="isLoading"
-        class="text-white absolute end-2.5 bottom-2.5 bg-primary focus:outline-none rounded text-lg font-bold px-6 py-2"
-        :class="{ 'bg-black-300': isLoading }"
-      >
-        {{ isLoading ? "Searching..." : "Search" }}
-      </button>
-    </div>
-  </form> -->
   <div class="w-full max-w-xl space-y-2 mx-auto">
     <form class="flex space-x-2" @submit="handleSubmit">
       <input
