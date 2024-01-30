0d646310f318a1ed204ae6d094b9a3fe077476eb
Yassin-Aru <jocarhisouka@gmail.com>
2024-01-30 18:39:43 +0100

    feat: update Not Found View page

--- client/src/views/NotFoundView.vue
+++ client/src/views/NotFoundView.vue
@@ -1,21 +1,18 @@
 <template>
   <main
-    class="min-h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+    class="h-fitt  flex items-center justify-center py-12"
   >
     <div class="max-w-md w-full space-y-8 text-center">
-      <h1 class="mt-6 text-4xl font-extrabold text-gray-900 dark:text-gray-100">
+      <h1 class="text-4xl font-extrabold text-gray-900 dark:text-gray-100">
         404 Not Found
       </h1>
       <p class="mt-2 text-lg text-gray-600 dark:text-gray-400">
         The page you're looking for doesn't exist.
       </p>
       <div class="mt-5">
-        <a
-          class="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-          href="#"
-        >
-          Go to Home
-        </a>
+        <button class="bg-white hover:bg-white-800 text-black  text-xl font-medium w-fit py-2 px-3 rounded-md">
+          <a href="/" class="w-full">Go Home</a>
+        </button>
       </div>
     </div>
   </main>
