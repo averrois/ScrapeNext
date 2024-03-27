63ae4b2f24fe90e6546e30894f19c407f0336bfe
Yassin-Aru <jocarhisouka@gmail.com>
2024-03-27 17:43:18 +0000

    wip: request is not passing throught proxy

--- server/lib/scrape/index.ts
+++ server/lib/scrape/index.ts
@@ -25,7 +25,14 @@ export async function scrapeAmazonProduct(
   const agent = new HttpsProxyAgent(proxyUrl);
 
   try {
-    const response = await axios.get(url);
+    const response =  await axios.get(url, {
+      httpsAgent: agent,
+      proxy: false,
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      },
+    });
 
     // Initialize Cheerio
     const $ = cheerio.load(response.data);
