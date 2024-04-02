a1f6a95b9f74cbc09f1560d85b1ad8e2d524984a
Yassin-Aru <jocarhisouka@gmail.com>
2024-04-02 17:52:54 +0000

    refactor: refactor files strcuture

---
+++ server/src/lib/controllers/productController.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import Product from "../database/models/product.model";
+import { User } from "../types";
+import { generateEmailBody, sendEmail } from "../nodemailer";
+
+export async function getAllProducts(req: Request, res: Response) {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching products");
+  }
+}
+
+export async function getProductById(productId: string) {
+  try {
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) return null;
+
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+  try {
+    const product = await Product.findById(productId);
+
+    if (!product) return;
+
+    const userExists = product.users.some((user: User) => user.email === userEmail);
+
+    if (!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = await generateEmailBody(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
+    }
+  } catch (error: any) {
+    console.log(error);
+  }
+}
